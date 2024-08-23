6bf9632dd6db211391ee69cf1b90a176a937c23b
lkh <a01024705367@gmail.com>
2024-08-23 15:42:41 +0900

    update

--- src/main/java/day1/Print.java
+++ src/main/java/day1/Print.java
@@ -2,13 +2,13 @@ package day1;
 
 public class Print {
 
-        // 주석 (comment) : 사람이 보기 위한 코멘트 컴퓨터는 무시한다.(한줄 주석)
-        // 여러줄 주석 : 전체 선택 컨트롤 + /
-        //        aaa;
-        //        bbb;
-        //        ccc;
-        //        ddd;
-    public static void main(String args[]){
+    // 주석 (comment) : 사람이 보기 위한 코멘트 컴퓨터는 무시한다.(한줄 주석)
+    // 여러줄 주석 : 전체 선택 컨트롤 + /
+    //        aaa;
+    //        bbb;
+    //        ccc;
+    //        ddd;
+    public static void main(String args[]) {
         // 프로그램 명령어(코드)는 위에서 아래로 순차실행된다.
         // 출력 : System.out.print() 괄호 안의 값을 콘솔에 출력
         // 출력 : System.out.print'ln'() 출력 후 줄바꿈을 해준다
@@ -21,10 +21,10 @@ public class Print {
         // 문자 ""로 표현
         System.out.println("이경현");
         // 더하기 + 기호
-        System.out.println(100+200);
+        System.out.println(100 + 200);
         // 문자 + 문자
         System.out.println("100 + 200 =" + "300");
-        System.out.println("100 + 200 =" +300);
+        System.out.println("100 + 200 =" + 300);
         System.out.println("100 + 200 = " + 100 + 200); //문자+숫자 숫자가 문자가 되어 이어붙이기 됨
         System.out.println("100 + 200 = " + (100 + 200));
 
@@ -32,6 +32,6 @@ public class Print {
         System.out.println("안녕하세요\n반갑습니다");
 
         System.out.println("2 X 1 = 2\n2 X 2 = 4\n2 X 3 = 6\n2 X 4 = 8\n2 X 5 = 10\n2 X 6 = 12\n2 X 7 = 14\n2 X 8 = 16\n2 X 9 = 18");
-        System.out.println("2 X 1 = " + (2*1) + "\n2 X 2 = " + (2*2) + "\n 2 X 3 = "+ (2*3));
+        System.out.println("2 X 1 = " + (2 * 1) + "\n2 X 2 = " + (2 * 2) + "\n 2 X 3 = " + (2 * 3));
     }
 }
--- src/main/java/day1/Variable.java
+++ src/main/java/day1/Variable.java
@@ -23,7 +23,6 @@ public class Variable {
         // (변수이름) = x;
 
 
-
         // 값을 변수에 담아야한다. = 로 담기
         // 문자 담기 = 문자 이름;
 
@@ -33,22 +32,54 @@ public class Variable {
         age = 60;
 
         // 변수 값을 꺼내 사용하기
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
-        System.out.println("안녕하세요 저는 "+ age + "살 "+ name+" 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
+        System.out.println("안녕하세요 저는 " + age + "살 " + name + " 입니다");
 
 
         // 변수 사용시 주의사항
         // 동일한 변수가 2개 이상 존재할 수 없음
         // 변수명이 숫자나 특수기호로 시작되어서는 안된다 (숫자는 뒤에 붙는건 가능)
         // 변수는 재활용 가능하며 반드시 = 의 왼쪽에 위치하는 것이 규칙
+
+        // 문제 풀어보기 변수 정의 + 출력
+        int i;
+        i = 100;
+        double d;
+        d = 3.14;
+        String s;
+        s = "안녕하세요";
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+
+        i = 200;
+        d = 10.5;
+        s = "반갑습니다.";
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+
+
+        // 값 바꾸기
+        int a = 5;
+        int b = 10;
+
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+
+        a = 10;
+        b = 5;
+
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
 
 
     }
