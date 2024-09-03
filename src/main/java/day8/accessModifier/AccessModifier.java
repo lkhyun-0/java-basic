60b6321650fffa67430eeff6979148af4403cd41
lkh <a01024705367@gmail.com>
2024-09-03 10:44:06 +0900

    AccessModifier

--- src/main/java/day8/accessModifier/AccessModifier.java
+++ src/main/java/day8/accessModifier/AccessModifier.java
@@ -9,5 +9,34 @@ public class AccessModifier {
         //protected : 상속관계 + 같은 폴더 안에 있는 객체끼리 사용가능 > 지금 필요 없음
         //default : 같은 폴더 안에 있는 객체끼리 사용가능 > 지금 필요 없음
         //private : 자신만 사용가능
+
+        Calculator c1 = new Calculator();
+        c1.num1 = 8;
+        c1.divide();
+        //c1.num2 = 1; 이게 안됨
+
+        c1.num1 = 20;
+        c1.divide();
+        // 여기서 0을 넣은게 문제의 원인임
+        // 모두 접근 가능한 public으로 설정해놨기 때문임
+
+        // private으로 Calculator의 데이터를 외부로부터 보호하게되면 다른곳에서는 변경할 수 없음
+
+        c1.num1 = 10;
+        //c1.num2 = 2; 이것도 안됨
+
+        // num을 세팅할 수 있게 만드는 메서드
+        // c1.setnum은 public이라 접근이 가능해짐
+        // 조건문을 활용해서 오류를 잡을 수 있음
+        c1.setNum1(20);
+        c1.setNum2(1);
+
+        c1.divide();
+
+        System.out.println(c1.getNum1()); //
+
+        // 객체변수는 객체 내의 모든 메서드에 영향을 주므로 외부의 접근을 일반적으로
+        // private으로 막는다.
+        // 변수에 접근이 필요하면 getter와 setter 메서드를 public으로 만들어 사용한다.
     }
 }
---
+++ src/main/java/day8/accessModifier/Calculator.java
@@ -0,0 +1,41 @@
+package day8.accessModifier;
+
+public class Calculator {
+    int num1 = 10;
+    private int num2 = 2;
+
+    // setter
+    // 명명규칙 set변수명() -> 카멜표기법 반드시 지켜줘야함
+    // setNum1()
+
+    // 메서드는 변수를 사용해서 작동하는게 대부분
+    // 객체의 변수에 잘못된 데이터가 들어오면 메서드가 고장난다.
+    // 객체의 변수는 함부로 바꾸면 안된다.
+    public void divide() {
+        System.out.println(num1 / num2); // public 안에 있는 자원
+    }
+
+    // num의 값을 설정할 수 있는 메서드를 설정하는 법
+    // 이 메서드는 public으로 열어둠
+    // Calculator가 가진 num의 세팅 값을 변경할 수 있음
+    public void setNum1(int num1) {
+        this.num1 = num1;
+
+    }
+
+    public void setNum2(int num2) {
+        if (num2 == 0) {
+            System.out.println("0은 나눌 수 없습니다. 기본값인 1로 세팅합니다.");
+            num2 = 1;
+        }
+        this.num2 = num2;
+
+    }
+    public int getNum1() {
+        return num1;
+    }
+
+}
+// getter
+// 명명규칙 get변수명() -> 카멜표기법 반드시 지켜줘야함
+// getNum1()
