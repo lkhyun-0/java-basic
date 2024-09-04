6c5af284cca45cb04a7aeede943b7b67ac09f337
lkh <a01024705367@gmail.com>
2024-09-04 17:51:34 +0900

    updatez

--- src/main/java/miniProject/MemberInformation.java
+++ src/main/java/miniProject/MemberInformation.java
@@ -1,13 +1,14 @@
 package miniProject;
 
 public class MemberInformation { // 로그인 정보 저장용
-    private  String memberID;
-    private  String memberPW;
-    private  String memberNN;
+    private String memberID;
+    private String memberPW;
+    private String memberNN;
 
     public String getMemberID() {
         return memberID;
     }
+
     public void setMemberID(String memberID) {
         this.memberID = memberID;
     }
@@ -15,12 +16,15 @@ public class MemberInformation { // 로그인 정보 저장용
     public String getMemberPW() {
         return memberPW;
     }
+
     public void setMemberPW(String memberPW) {
         this.memberPW = memberPW;
     }
+
     public String getMemberNN() {
         return memberNN;
     }
+
     public void setMemberNN(String memberNN) {
         this.memberNN = memberNN;
     }
--- src/main/java/miniProject/PostRun.java
+++ src/main/java/miniProject/PostRun.java
@@ -2,7 +2,7 @@ package miniProject;
 
 public class PostRun { // 메인에 스위치 메서드
     public static void main(String[] args) {
-    PostApp postApp = new PostApp();
-    postApp.run();
+        PostApp postApp = new PostApp();
+        postApp.run();
     }
 }
