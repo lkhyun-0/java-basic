78a9a5e9e85d7271ad4f6a3b65796639a9893692
lkh <a01024705367@gmail.com>
2024-09-05 17:02:09 +0900

    all

--- src/main/java/miniProject/PostView.java
+++ src/main/java/miniProject/PostView.java
@@ -15,15 +15,11 @@ public class PostView {
             System.out.println("==================");
         }
     }
-
     public void printPostDetail(Post post) {
         System.out.printf("번호 : %d\n", post.getId());
         System.out.printf("제목 : %s\n", post.getTitle());
         System.out.printf("내용 : %s\n", post.getBody());
         System.out.printf("등록날짜 : %s\n", post.getCreateDate());
         System.out.printf("조회수 : %d\n", post.getHit());
-
     }
-
-
 }
